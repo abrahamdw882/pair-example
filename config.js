1fc0ad4473a4d466769eb4f515fd3c1a59319890
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-10-21 17:07:10 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  PREFIX: process.env.PREFIX || "IRON-M4N×",
-  EMAIL: process.env.EMAIL || "ironman@onlyfans.com",
-  PASS: process.env.PASS || "Kimi da yo kimi nandayo..",
+  PREFIX: process.env.PREFIX || "ASTRO×",
+  EMAIL: process.env.EMAIL || "abrahamdw882@gmail.com",
+  PASS: process.env.PASS || "#abbotdevi1",
   MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id blah blah .etc",
-  IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg",
+  IMAGE: process.env.IMAGE || "https://i.ibb.co/Mh87DFd/633c524b-3e4e-40ed-8ad8-485b36800d5d.jpg",
 }
